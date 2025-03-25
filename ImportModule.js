3c01609ed1df57953a2460b56ad5cf7449f502f9
isuruamarasooriya <isuruamarasooriya177@gmail.com>
2025-03-26 00:19:39 +0530

    Create ImportModule.js

---
+++ ImportModule.js
@@ -0,0 +1,9 @@
+let student = [
+    {RegNo: "2021ICT42", Name: "Isuru Amarasooriya", Age: 24, Gender: "Male"},
+    {RegNo: "2021ICT122", Name: "Kanishka", Age: 24, Gender: "Male"},
+    {RegNo: "2021ICT03", Name: "John Smith", Age: 23, Gender: "Male"},
+    {RegNo: "2021ICT35", Name: "Imiyah", Age: 24, Gender: "Female"},
+    {RegNo: "2021ICT08", Name: "Praveen", Age: 23, Gender: "Male"},
+];
+
+module.exports=student;
